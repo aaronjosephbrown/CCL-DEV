7129a4102f599430c66f44caea284ec8ee54785f
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-04-03 10:08:27 -0500

    Updated file structure to match the new structure of the project.

--- anesthesia/actions-by-year.sql
+++ anesthesia/actions-by-year.sql
@@ -1,3 +1,4 @@
+-- Purpose: Build a list of actions charted on anesthesia records by year
 SELECT
   sa.record_description,
   sa.create_dt_tm,
@@ -10,21 +11,28 @@ FROM
   sa_ref_action sra,
   sa_macro sm,
   sa_ref_macro srm 
+  
   PLAN sa
   JOIN saa
 WHERE
   sa.sa_anesthesia_record_id = saa.sa_anesthesia_record_id
-  AND sa.record_description NOT LIKE 'CE%'
+  -- Remove the record description from the list if it starts with CE
+  AND sa.record_description NOT LIKE 'CE*'
+  -- Only include records in 2022 and 2023 
   AND YEAR (sa.create_dt_tm) IN (2022, 2023)
+
   JOIN sra
 WHERE
   saa.sa_ref_action_id = sra.sa_ref_action_id
+
   JOIN sm
 WHERE
   saa.sa_macro_id = sm.sa_macro_id
+
   JOIN srm
 WHERE
   sm.sa_ref_macro_id = srm.sa_ref_macro_id
+  
 WITH
   TIME = 180,
   MAXREC = 100000;
